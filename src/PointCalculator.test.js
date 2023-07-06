a4d6330b0c6ec15360f85bf84688879ffcc427fe
F.Kanayama <fkanayam@redhat.com>
2023-07-06 18:31:11 +0900

    Add skelton src files

---
+++ src/PointCalculator.test.js
@@ -0,0 +1,20 @@
+import {pointCalculator} from './PointCalculator';
+
+describe('pointCalculator', () => {
+    it('xxx', () => {
+        expect(true).toBe(true);
+    });
+})
+
+
+
+// Unit Test Demo //////////////////////
+function myThreeTimes(n) {
+  return n * 3;
+}
+
+describe('myThreeTimes', () => {
+    it('引数を3倍にして返す', () => {
+        expect(myThreeTimes(1)).toBe(3);
+    });
+})
