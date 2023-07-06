718318d7e9ce027f2f8de521a373ab617aa928c9
F.Kanayama <fkanayam@redhat.com>
2023-07-07 00:23:28 +0900

    Example answer

--- src/PrintingPageParser.js
+++ src/PrintingPageParser.js
@@ -1,6 +1,81 @@
+/*
+1. 引数として文字列を受け取り、印刷対象ページの数値のリストを返す関数を作成する。
+2. 引数が単独の数値のみである場合、その数値を格納したリストを返す。
+3. 引数がハイフン繋ぎの数値である場合、FromからToまでの数値を展開したリストを返す。但し、From＞Toの場合はエラー。
+4. 引数がカンマ区切りである場合、カンマで区切られた各要素のFromからToまでの数値を展開したリストを返す。
+5. 数値リストは昇順（１ -> 100）でソートされている。
+6. 但し、重複は一意化する。
+7. 引数が未指定の場合、例外をスローする。
+8. 引数が数値またはハイフンまたはカンマでない場合、例外をスローする。
+9. 数値に1未満の値が含まれる場合、例外をスローする。
+10. “--”や",,"などは例外をスローする
+11. -nは1ページ目からnページまでのリストを返す。
 
-function parsePrintingPage() {
+* 例外はErrorオブジェクトをthrowする
 
+*/
+
+function parsePrintingPage(pages) {
+    if (pages === undefined || pages === '') {
+        throw new Error('The argument is empty.');
+    }
+    if (doesIncludeInvalidChar(pages)) {
+        throw new Error('The argument includes invalid char.');
+    }
+
+    let ret = []
+    pages.split(',').forEach(csv_elem => {
+        if (csv_elem === '') {
+            throw new Error('The argument is empty.');
+        }
+        parsePrintingPageListElement(csv_elem).forEach(page_num => {
+            ret.push(page_num);
+        });
+    });
+    
+    ret = unique(ret).sort((a, b) => a - b);
+    ret.map(elem => {
+        if (elem < 1) { throw new Error('The argument includes invalid page number.'); }
+    });
+    return ret;
 }
 
-export {parsePrintingPage}
+function parsePrintingPageListElement(pages) {
+    if (pages.includes('-')) {
+        const from_and_to = pages.split('-').map(elem => parseInt(elem));
+        if (from_and_to.length !== 2) {
+            throw new Error('The argument includes invalid value');
+        }
+        let [from, to] = from_and_to;
+        if (Number.isNaN(from)) {
+            from = 1;
+        }
+        if (from > to) {
+            throw new Error('from > to');
+        }
+        const length = to - from + 1;
+        return [...Array(length).keys()].map((_, i) => i + from);
+    } else {
+        return [parseInt(pages)]
+    }
+}
+
+// 引数のリストを一意化する
+function unique(list) {
+    const set = new Set(list);
+    return Array.from(set);
+}
+
+// 引数の文字列pagesが(半角数字 or マイナス記号 or カンマ or 半角スペース)以外の文字を含んでいる場合trueを返す
+function doesIncludeInvalidChar(pages) {
+    const regex = /[^0-9,\- ]/;
+    return regex.test(pages);
+}
+
+
+export {
+    parsePrintingPage,
+    parsePrintingPageListElement,
+    unique,
+    doesIncludeInvalidChar
+}
--- src/PrintingPageParser.test.js
+++ src/PrintingPageParser.test.js
@@ -1,21 +1,83 @@
-import {parsePrintingPage} from './PrintingPageParser';
+import {
+    parsePrintingPage,
+    parsePrintingPageListElement,
+    unique,
+    doesIncludeInvalidChar,
+} from './PrintingPageParser';
 
 describe('parsePrintingPage', () => {
-    it('xxx', () => {
-        expect(parsePrintingPage('')).toBe();
+    it('2. 引数が単独の数値のみである場合、その数値を格納したリストを返す。', () => {
+        expect(parsePrintingPageListElement('1')).toStrictEqual([1]);
+        expect(parsePrintingPageListElement('123')).toStrictEqual([123]);
+        expect(parsePrintingPageListElement('  123   ')).toStrictEqual([123]);
+    });
+
+    it('3. 引数がハイフン繋ぎの数値である場合、FromからToまでの数値を展開したリストを返す。但し、From＞Toの場合はエラー。', () => {
+        expect(parsePrintingPageListElement('1-3')).toStrictEqual([1, 2, 3]);
+        expect(parsePrintingPageListElement('102-105')).toStrictEqual([102, 103, 104, 105]);
+
+        expect(() => parsePrintingPageListElement('5-4')).toThrowError();
+    });
+
+    it('4. 引数がカンマ区切りである場合、カンマで区切られた各要素のFromからToまでの数値を展開したリストを返す。但し、重複は一意化する。', () => {
+        expect(parsePrintingPage('1, 3')).toStrictEqual([1, 3]);
+        expect(parsePrintingPage('1, 3-5')).toStrictEqual([1, 3, 4, 5]);
+        expect(parsePrintingPage('10, 1-3')).toStrictEqual([1, 2, 3, 10]);
+    });
+
+    it('5. 数値リストは昇順（１ -> 100）でソートされている。', () => {
+        expect(parsePrintingPage('3,2,1,4')).toStrictEqual([1,2,3,4]);
+    });
+
+    it('6. 重複は一意化する。', () => {
+        expect(unique([1,1])).toStrictEqual([1]);
+    });
+
+    it('7. 引数が未指定の場合、例外をスローする。', () => {
+        expect(() =>parsePrintingPage()).toThrow();
+        expect(() =>parsePrintingPage('')).toThrow();
+    });
+
+    it('8. 引数が数値またはハイフンまたはカンマでない場合、例外をスローする。', () => {
+        expect(doesIncludeInvalidChar('1, 2-3')).toBeFalsy();
+        expect(doesIncludeInvalidChar('1,,,,,----')).toBeFalsy();
+
+        expect(doesIncludeInvalidChar('abc')).toBeTruthy();
+        expect(doesIncludeInvalidChar('+')).toBeTruthy();
+        expect(doesIncludeInvalidChar('あいうえお')).toBeTruthy();
+    });
+
+    it('9. 数値に1未満の値が含まれる場合、例外をスローする。', () => {
+        expect(() => parsePrintingPage('0')).toThrow();
+        expect(() => parsePrintingPage('0.1')).toThrow();
+        //expect(() => parsePrintingPage('-1')).toThrow();
+    });
+
+    it('10. “--”や",,"などは例外をスローする', () => {
+        expect(() => parsePrintingPage(',,')).toThrow();
+        expect(() => parsePrintingPage('-')).toThrow();
+        expect(() => parsePrintingPage('--')).toThrow();
+        expect(() => parsePrintingPage('2--4')).toThrow();
+        expect(() => parsePrintingPage('2-4-6')).toThrow();
+        //expect(() => parsePrintingPage('-6')).toThrow();
+        expect(() => parsePrintingPage('6-')).toThrow();
+    });``
+
+    it('11. -nは1ページ目からnページまでのリストを返す。', () => {
+        expect(parsePrintingPage('-3')).toStrictEqual([1, 2, 3]);
     });
     
 })
 
 
 
-// Unit Test Demo //////////////////////
-function myThreeTimes(n) {
-  return n * 3;
-}
+// // Unit Test Demo //////////////////////
+// function myThreeTimes(n) {
+//   return n * 3;
+// }
 
-describe('myThreeTimes', () => {
-    it('引数を3倍にして返す', () => {
-        expect(myThreeTimes(1)).toBe(3);
-    });
-})
+// describe('myThreeTimes', () => {
+//     it('引数を3倍にして返す', () => {
+//         expect(myThreeTimes(1)).toBe(3);
+//     });
+// })
