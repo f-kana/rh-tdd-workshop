ba6ee346d07e085bce4ca8a251fde079cce90378
F.Kanayama <fkanayam@redhat.com>
2023-07-07 16:13:44 +0900

    fix skelton

---
+++ src/PrintingPageParser.js
@@ -0,0 +1,6 @@
+
+function pointCalculator() {
+  
+}
+
+export {pointCalculator}
---
+++ src/PrintingPageParser.test.js
@@ -0,0 +1,20 @@
+import {parsePrintingPage} from './PrintingPageParser';
+
+describe('parsePrintingPage', () => {
+    it('xxx', () => {
+        expect(true).toBe(true);
+    });
+})
+
+
+
+// Unit Test Demo //////////////////////
+function myThreeTimes(n) {
+  return n * 3;
+}
+
+describe('myThreeTimes', () => {
+    it('引数を3倍にして返す', () => {
+        expect(myThreeTimes(1)).toBe(3);
+    });
+})
