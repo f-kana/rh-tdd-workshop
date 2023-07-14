ee8ae2901e6a5b468751bc69a4369fd16f3ad40f
F.Kanayama <fkanayam@redhat.com>
2023-07-14 18:00:56 +0900

    at workshop end

--- src/PrintingPageParser.js
+++ src/PrintingPageParser.js
@@ -1,6 +1,57 @@
+/*
 
-function parsePrintingPage() {
+Done
+1. 引数としてページ数を示す文字列と、MAXページ数を受け取り、印刷対象ページの数値のリストを返す関数を作成する。
+2. 単独の数字を入力→その数字を印刷対象ページとして返す
+3. カンマ区切りを処理する＋昇順
+4. 重複削除
+5. 範囲指定（ハイフン区切り）の正常系（テストは複合系を含む）
+6. ゼロパディング
+7. トリム（半角スペース）
+8. エラー：入力無し（引数無し、空文字）
+9. エラー：OK以外（全角数字、全角カンマなど含め）
 
+ToDo
+10. エラー：「,,」、「--」などのケース
+12. 「1      5」→ERROR
+13. エラー：範囲指定の「開始番号（From）」が「終了番号（To）」より大きい場合
+14. OKケース、エラーケースを主つくままに。
+
+15. 「-6」 -> 1,2,3,4,5,6　「10-」 10から最後まで
+
+*/
+function parsePrintingPage(pages, maxPage) {
+    let printingPages = [];
+
+    if (pages === undefined || pages === "") {
+        throw new Error("数値が指定されていません。");
+    }
+    if (doesIncludeInvalidChar(pages)) {
+        throw new Error("不正な文字列が使われています。");
+    }
+
+    pages.split(",").forEach(splittedPage => {
+        if (splittedPage.includes("-")) {
+            const [startPage, endPage] = splittedPage.split("-").map((hyphenSplittedPage) => Number(hyphenSplittedPage));
+            printingPages = range(startPage, endPage);
+
+        } else {
+            printingPages.push(Number(splittedPage));
+        }
+    })
+
+    return Array.from(new Set(printingPages)).sort((a, b) => a - b)
 }
 
-export {parsePrintingPage}
+function range(startPage, endPage) {
+    const len = endPage - startPage + 1;
+    return  [...Array(len).keys()].map((_, i) => i + startPage);
+}
+
+function doesIncludeInvalidChar(pages) {
+    const regex = /[^0-9,\- ]/;
+    return regex.test(pages);
+}
+
+
+export {parsePrintingPage, range}
--- src/PrintingPageParser.test.js
+++ src/PrintingPageParser.test.js
@@ -1,20 +1,73 @@
-import {parsePrintingPage} from './PrintingPageParser';
+import {parsePrintingPage, range} from './PrintingPageParser';
 
 describe('parsePrintingPage', () => {
-    it('xxx', () => {
-        expect(true).toBe(true);
+    it('2. 単独の数字を入力→その数字を印刷対象ページとして返す', () => {
+        expect(parsePrintingPage("1", 100)).toStrictEqual([1]);
+        expect(parsePrintingPage("100", 100)).toStrictEqual([100]);
     });
+
+    it('3. カンマ区切りを処理する＋昇順', () => {
+        expect(parsePrintingPage("1,2", 100)).toStrictEqual([1,2]);
+        expect(parsePrintingPage("2,1", 100)).toStrictEqual([1,2]);
+        expect(parsePrintingPage("2,10,1", 100)).toStrictEqual([1,2,10]);
+    });
+
+    it('4. 重複削除', () => {
+        expect(parsePrintingPage("1,2,2,3", 100)).toStrictEqual([1,2,3]);    });
+
+    it('5. 範囲指定（ハイフン区切り）の正常系（テストは複合系を含む）', () => {
+        expect(parsePrintingPage("1-3", 100)).toStrictEqual([1,2,3]);
+        expect(parsePrintingPage("1-3,4", 100)).toStrictEqual([1,2,3,4]);
+    });
+
+    it('range', () => {
+        expect(range(1, 3)).toStrictEqual([1,2,3]);
+    });
+
+    it('6. ゼロパディング', () => {
+        expect(parsePrintingPage("01,002", 100)).toStrictEqual([1, 2]);
+    });
+
+    it('7. トリム（半角スペース）', () => {
+        expect(parsePrintingPage("1, 2", 100)).toStrictEqual([1, 2]);
+        expect(parsePrintingPage(" 1 - 2 ", 100)).toStrictEqual([1,2]);
+    });
+
+    it('8. エラー：入力無し', () => {
+        expect(() => parsePrintingPage()).toThrowError();
+        expect(() => parsePrintingPage("", 100)).toThrowError();
+    });
+
+    it('9. エラー：OK以外（全角数字、全角カンマなど含め）', () => {
+        expect(() => parsePrintingPage("あいうえお", 100)).toThrowError();
+        expect(() => parsePrintingPage("一", 100)).toThrowError();
+        expect(() => parsePrintingPage("、", 100)).toThrowError();
+        expect(() => parsePrintingPage("１", 100)).toThrowError();
+        expect(() => parsePrintingPage("a", 100)).toThrowError();
+        expect(() => parsePrintingPage("−", 100)).toThrowError();
+        expect(() => parsePrintingPage("ー", 100)).toThrowError();
+        expect(() => parsePrintingPage("/", 100)).toThrowError();
+        expect(() => parsePrintingPage("　", 100)).toThrowError();
+        expect(() => parsePrintingPage("1.1", 100)).toThrowError();
+    });
+
+    // it('', () => {
+    //     expect(parsePrintingPage()).toStrictEqual();
+    //     expect(parsePrintingPage()).toStrictEqual();
+    // });
 })
 
 
 
-// Unit Test Demo //////////////////////
+//Unit Test Demo //////////////////////
 function myThreeTimes(n) {
-  return n * 3;
+  return n * 4;
 }
 
 describe('myThreeTimes', () => {
     it('引数を3倍にして返す', () => {
-        expect(myThreeTimes(1)).toBe(3);
+        expect(myThreeTimes(1)).toBe(4);
+        expect(myThreeTimes(-2.1)).toBe(-8.4);
+        expect(myThreeTimes(0)).toBe(0);
     });
 })
