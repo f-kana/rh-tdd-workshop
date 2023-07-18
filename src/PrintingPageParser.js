8e4b1d8db9609cb4a80ce7d8fc6961df4ff465f6
F.Kanayama <fkanayam@redhat.com>
2023-07-18 18:28:02 +0900

    save

--- src/PrintingPageParser.js
+++ src/PrintingPageParser.js
@@ -1,6 +1,47 @@
 
-function parsePrintingPage() {
+/*
 
+1引数としてページ数を示す文字列を受け取り、印刷対象ページの数値のリストを返す関数を作成する。
+2単独数字が入力されたら、要素数１（入力された数字）の数字のリストを返す。
+3カンマ区切りが入力されたら、数字のリストを返す
+4ハイフン区切り
+5重複削除
+6昇順
+7入力無し→エラー
+8（0123456789,- )か否かを判別し、数値でなければエラー。
+9単独はダメ。
+10カンマの前後が数字じゃない、ハイフンの前後が数字じゃない　→エラー
+
+11. "-3"　ー＞最初のページから３ぺーじ目まで印刷
+11. "3-"　ー＞３ぺーじ目から最終ページまで印刷
+
+*/
+
+function parsePrintingPage(pages) {
+    let printPages = [];
+    pages.split(',').forEach(function (value) {
+        if (value.includes('-')) {
+            let [startVal, endVal] = value.split('-').map(Number);
+            if (startVal > endVal) {
+                [startVal, endVal] = [endVal, startVal];
+            }
+
+            printPages = printPages.concat(myRange(startVal, endVal))
+        } else {
+            printPages = printPages.concat(value.split(',').map(Number));
+        }
+    });
+
+    return printPages;
+    
+}
+
+function myRange(startVal, endVal) {
+    let ret = [];
+    for(let i = startVal; i <= endVal; i++) {
+        ret.push(i)
+    }
+    return ret;
 }
 
 export {parsePrintingPage}
--- src/PrintingPageParser.test.js
+++ src/PrintingPageParser.test.js
@@ -1,20 +1,53 @@
 import {parsePrintingPage} from './PrintingPageParser';
 
 describe('parsePrintingPage', () => {
-    it('xxx', () => {
-        expect(true).toBe(true);
+    it('2単独数字が入力されたら、要素数１（入力された数字）の数字のリストを返す。', () => {
+        expect(parsePrintingPage("1")).toStrictEqual([1]);
+        expect(parsePrintingPage("12")).toStrictEqual([12]);
     });
+
+    it('3カンマ区切りが入力されたら、数字のリストを返す', () => {
+        expect(parsePrintingPage("1,3")).toStrictEqual([1,3]);
+    });
+
+    it('4-1 ハイフン区切り 1-3', () => {
+        expect(parsePrintingPage("1-3")).toStrictEqual([1,2,3]);
+    });
+
+    it('4-2 ハイフン区切りとカンマ区切りの複合系 1-3,5', () => {
+        expect(parsePrintingPage("1-3,5")).toStrictEqual([1,2,3,5]);
+    });
+
+    it('4-3 ハイフン区切り 逆転 3-1', () => {
+        expect(parsePrintingPage("3-1")).toStrictEqual([1,2,3]);
+    });
+
+    // it('4-4 ハイフン区切り 逆転 複合 3-1', () => {
+    //     expect(parsePrintingPage("3-1,5")).toStrictEqual([1,2,3,5]);
+    // });
+
+    // it('', () => {
+    //     expect(parsePrintingPage(???)).toStrictEqual(???);
+    // });
+
+    // it('', () => {
+    //     expect(parsePrintingPage(???)).toStrictEqual(???);
+    // });
+
+    // it('', () => {
+    //     expect(parsePrintingPage(???)).toStrictEqual(???);
+    // });
 })
 
 
 
 // Unit Test Demo //////////////////////
-function myThreeTimes(n) {
-  return n * 3;
-}
+// function myThreeTimes(n) {
+//   return n * 4;
+// }
 
-describe('myThreeTimes', () => {
-    it('引数を3倍にして返す', () => {
-        expect(myThreeTimes(1)).toBe(3);
-    });
-})
+// describe('myThreeTimes', () => {
+//     it('引数を3倍にして返す', () => {
+//         expect(myThreeTimes(1)).toBe(4);
+//     });
+// })
