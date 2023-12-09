7e1500a7a6cd3998481d20b42168b22931500883
F.Kanayama <fkanayam@redhat.com>
2023-12-09 15:41:31 +0900

    save

---
+++ src/test_printing_page_parser.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from .printing_page_parser import parse_printing_page
+
+
+class GeneralTest(TestCase):
+    def test_parse_printing_page(self):
+        with self.subTest("1.引数が単独の正の数(文字列型)のみの場合、その数値(Number型)を格納したリストを返す。"):
+            self.assertListEqual(parse_printing_page("1"), [1])
+            self.assertListEqual(parse_printing_page("10"), [10])
+
+        with self.subTest("2.引数がハイフン繋ぎの数字の場合、FromからToまでの数値を展開したリストを返す。"):
+            # FromとToが連番 or 離れているのか
+            self.assertListEqual(
+                parse_printing_page("1-10"), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            )
+            self.assertListEqual(parse_printing_page("2-3"), [2, 3])
+
+        with self.subTest("3-1.引数がカンマ区切りの数字の場合、カンマで区切られた各要素の数値を展開したリストを返す。"):
+            # カンマが単体か複数か
+            # 順子、嵌張
+            self.assertListEqual(parse_printing_page("1,10,20"), [1, 10, 20])
+            self.assertListEqual(parse_printing_page("2,3"), [2, 3])
+
+        # with self.subTest("3-2.カンマとハイフンの混合"):
+        #     # テスト観点
+        #     # * ハイフン含みが先にくるか後にくるか
+        #     self.assertListEqual(parse_printing_page("1-2,3"), [1, 2, 3])
+        #     self.assertListEqual(parse_printing_page("1,2-3,5-7"), [1, 2, 3, 5, 6, 7])
+
+        # with self.subTest("xxx"):
+        #     self.assertListEqual(parse_printing_page(None), None)
+
+        # with self.subTest("xxx"):
+        #     self.assertListEqual(parse_printing_page(None), None)
+
+
+# def my_three_times(n):
+#     return 3 * n
+
+
+# class DemoTest(TestCase):
+#     def test_my_three_times(self):
+#         with self.subTest("入力の数値が3倍になる。"):
+#             self.assertEqual(my_three_times(1), 3)
