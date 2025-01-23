ab7c4ca2a63594507e3449600673565f42ac4c52
F.Kanayama <fkanayam@redhat.com>
2025-01-23 09:22:22 +0900

    save

--- src/printing_page_parser.py
+++ src/printing_page_parser.py
@@ -1,9 +1,19 @@
 """
 ToDo:
-1. xxx
-2. xxx
 """
 
 
-def parse_printing_page():
-    return
+def parse_printing_page(input_page: str) -> list[int]:
+    if input_page.isdigit():
+        if len(input_page) == 5:
+            raise ValueError("5桁の数値は入力できません。")
+        return [int(input_page)]
+
+    if "-" in input_page:
+        from_page, to_page = map(int, input_page.split("-"))
+        return list(range(from_page, to_page + 1))
+
+    if "," in input_page:
+        return list(map(int, input_page.split(",")))
+
+    raise ValueError("入力が不正です。")
--- src/test_printing_page_parser.py
+++ src/test_printing_page_parser.py
@@ -5,18 +5,32 @@ from .printing_page_parser import parse_printing_page
 
 class GeneralTest(TestCase):
     def test_parse_printing_page(self):
-        with self.subTest("xxx"):
-            # self.assertListEqual(parse_printing_page(), None)
-            self.assertEqual(parse_printing_page(), None)
-        # with self.subTest("xxx"):
-        #     self.assertListEqual(parse_printing_page(None), None)
+        with self.subTest(
+            "1. 引数が単独の正の数(文字列型)のみの場合、その数値(Number型)を格納したリストを返す。ただし入力が５桁→エラー"
+        ):
+            self.assertListEqual(parse_printing_page("1"), [1])
+            self.assertListEqual(parse_printing_page("3"), [3])
+            self.assertListEqual(parse_printing_page("9999"), [9999])
+            # self.assertRaises(parse_printing_page("10000"), Exception)
+
+        with self.subTest("2. 引数がカンマ区切りの数字の場合、カンマで区切られた各要素の数値を展開したリストを返す。"):
+            # カンマの数が1、2以上
+            # 順子、嵌張
+            # self.assertListEqual(parse_printing_page("1,2"), [1, 2])
+            self.assertListEqual(parse_printing_page("1,3"), [1, 3])
+            self.assertListEqual(parse_printing_page("1,2,3"), [1, 2, 3])
+            # self.assertListEqual(parse_printing_page("1,3,5"), [1, 3, 5])
+
+        with self.subTest("3. 引数がハイフン繋ぎの数字の場合、FromからToまでの数値を展開したリストを返す。"):
+            self.assertListEqual(parse_printing_page("1-2"), [1, 2])
+            self.assertListEqual(parse_printing_page("1-5"), [1, 2, 3, 4, 5])
 
 
-def my_three_times(n):
-    return 3 * n
+# def my_three_times(n):
+#     return 3 * n
 
 
-class DemoTest(TestCase):
-    def test_my_three_times(self):
-        with self.subTest("入力の数値が3倍になる。"):
-            self.assertEqual(my_three_times(1), 3)
+# class DemoTest(TestCase):
+#     def test_my_three_times(self):
+#         with self.subTest("入力の数値が3倍になる。"):
+#             self.assertEqual(my_three_times(1), 3)
