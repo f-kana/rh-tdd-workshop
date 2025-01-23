00ad7046c877dbffc0f0c8f11e76a815621261ed
F.Kanayama <fkanayam@redhat.com>
2025-01-23 15:13:31 +0900

    fix ai comment

--- src/test_printing_page_parser.py
+++ src/test_printing_page_parser.py
@@ -11,16 +11,11 @@ class GeneralTest(TestCase):
             self.assertListEqual(parse_printing_page("1"), [1])
             self.assertListEqual(parse_printing_page("3"), [3])
             self.assertListEqual(parse_printing_page("9999"), [9999])
-            # self.assertRaises(parse_printing_page("10000"), Exception)
-
         with self.subTest("2. 引数がカンマ区切りの数字の場合、カンマで区切られた各要素の数値を展開したリストを返す。"):
             # カンマの数が1、2以上
             # 順子(シュンツ)、嵌張(カンチャン)
-            # self.assertListEqual(parse_printing_page("1,2"), [1, 2])
             self.assertListEqual(parse_printing_page("1,3"), [1, 3])
             self.assertListEqual(parse_printing_page("1,2,3"), [1, 2, 3])
-            # self.assertListEqual(parse_printing_page("1,3,5"), [1, 3, 5])
-
         with self.subTest("3. 引数がハイフン繋ぎの数字の場合、FromからToまでの数値を展開したリストを返す。"):
             self.assertListEqual(parse_printing_page("1-2"), [1, 2])
             self.assertListEqual(parse_printing_page("1-5"), [1, 2, 3, 4, 5])
@@ -32,13 +27,3 @@ class GeneralTest(TestCase):
             self.assertListEqual(parse_printing_page("1-5,7-8"), [1, 2, 3, 4, 5, 7, 8])  # ハイフン2、ハイフン先
             self.assertListEqual(parse_printing_page("1,5-7"), [1, 5, 6, 7])  # ハイフン1、カンマ先
             self.assertListEqual(parse_printing_page("1,2-3,4-5"), [1, 2, 3, 4, 5])  # ハイフン2、カンマ先
-
-
-# def my_three_times(n):
-#     return 3 * n
-
-
-# class DemoTest(TestCase):
-#     def test_my_three_times(self):
-#         with self.subTest("入力の数値が3倍になる。"):
-#             self.assertEqual(my_three_times(1), 3)
