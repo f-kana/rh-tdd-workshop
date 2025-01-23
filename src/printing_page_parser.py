aaacd68bcca294fc9832cfaa7d8cd8b3b745826f
F.Kanayama <fkanayam@redhat.com>
2025-01-23 14:56:39 +0900

    implement todo 3.1

--- src/printing_page_parser.py
+++ src/printing_page_parser.py
@@ -3,17 +3,12 @@ ToDo:
 """
 
 
-def parse_printing_page(input_page: str) -> list[int]:
-    if input_page.isdigit():
-        if len(input_page) == 5:
-            raise ValueError("5桁の数値は入力できません。")
-        return [int(input_page)]
-
-    if "-" in input_page:
-        from_page, to_page = map(int, input_page.split("-"))
-        return list(range(from_page, to_page + 1))
-
-    if "," in input_page:
-        return list(map(int, input_page.split(",")))
-
-    raise ValueError("入力が不正です。")
+def parse_printing_page(input_pages: str) -> list[int]:
+    output_page = []
+    for item in input_pages.split(","):
+        if "-" in item:
+            [start, end] = map(int, item.split("-"))
+            output_page.extend(list(range(start, end + 1)))
+        else:
+            output_page.append(int(item))
+    return output_page
--- src/test_printing_page_parser.py
+++ src/test_printing_page_parser.py
@@ -15,7 +15,7 @@ class GeneralTest(TestCase):
 
         with self.subTest("2. 引数がカンマ区切りの数字の場合、カンマで区切られた各要素の数値を展開したリストを返す。"):
             # カンマの数が1、2以上
-            # 順子、嵌張
+            # 順子(シュンツ)、嵌張(カンチャン)
             # self.assertListEqual(parse_printing_page("1,2"), [1, 2])
             self.assertListEqual(parse_printing_page("1,3"), [1, 3])
             self.assertListEqual(parse_printing_page("1,2,3"), [1, 2, 3])
@@ -24,6 +24,14 @@ class GeneralTest(TestCase):
         with self.subTest("3. 引数がハイフン繋ぎの数字の場合、FromからToまでの数値を展開したリストを返す。"):
             self.assertListEqual(parse_printing_page("1-2"), [1, 2])
             self.assertListEqual(parse_printing_page("1-5"), [1, 2, 3, 4, 5])
+
+        with self.subTest("3.1. ハイフンとカンマが混在"):
+            # カンマが先、ハイフンが先か
+            # ハイフン1と2以上
+            self.assertListEqual(parse_printing_page("1-2,3"), [1, 2, 3])  # ハイフン1、ハイフン先
+            self.assertListEqual(parse_printing_page("1-5,7-8"), [1, 2, 3, 4, 5, 7, 8])  # ハイフン2、ハイフン先
+            self.assertListEqual(parse_printing_page("1,5-7"), [1, 5, 6, 7])  # ハイフン1、カンマ先
+            self.assertListEqual(parse_printing_page("1,2-3,4-5"), [1, 2, 3, 4, 5])  # ハイフン2、カンマ先
 
 
 # def my_three_times(n):
